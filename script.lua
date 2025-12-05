42389d8b45087aa8ef975722ecfa2e1825a941ad
oberondart <durustein@gmail.com>
2025-12-05 15:36:56 -0500

    script.lua

--- script.lua
+++ script.lua
@@ -1,13 +1,11 @@
 local nova = require("nova")
 
-nova.let("my_number", 123) -- declare variables
+nova.let("my_number", 123)
 nova.let("lucky", 12)
 nova.let("counter", 0)
 
-nova.out("my_number") -- standard output via out()
+nova.out("my_number")
 nova.out("lucky")
 
-nova.loop(3, function() -- loop, not entirely sufficent, you need an anonymous function
-    nova.incr("counter")
-    nova.out("counter")
-end)
+nova.array("my_array", {1, 2, 3, 4, 5})
+nova.out("my_array")
