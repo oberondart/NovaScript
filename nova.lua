201ee2d1e4aede38f3da5c3f625ef043dc2e830b
oberondart <durustein@gmail.com>
2026-01-01 09:27:18 -0500

    Update nova.lua

--- nova.lua
+++ nova.lua
@@ -121,11 +121,11 @@ function Nova.call(name, ...)
     end
 end
 
-function Nova.ifclause(varname, value, statement, elseclause)
-    if varstorage[varname] == value then
-        Nova.out(statement)
-    else
-        Nova.out(elseclause)
+function Nova.ifdo(name, value, then_fn, else_fn)
+    if varstorage[name] == value then
+        then_fn()
+    elseif else_fn then
+        else_fn()
     end
 end
 
@@ -192,6 +192,70 @@ function Nova.arraylength(name)
         return
     end
     Nova.out(#a)
+end
+
+function Nova.add(x, y)
+    x = varstorage[x] or x
+    y = varstorage[y] or y
+    return x + y
+end
+
+function Nova.sub(x, y)
+    x = varstorage[x] or x
+    y = varstorage[y] or y
+    return x - y
+end
+
+function Nova.mul(x, y)
+    x = varstorage[x] or x
+    y = varstorage[y] or y
+    return x * y
+end
+
+function Nova.div(x, y)
+    x = varstorage[x] or x
+    y = varstorage[y] or y
+    if y == 0 then
+        print("error: division by zero")
+        return
+    end
+    return x / y
+end
+
+function Nova.len(inputstring)
+    inputstring = varstorage[inputstring] or inputstring
+    if type(inputstring) ~= "string" then
+        print("error: len() expects type string")
+        return
+    end
+    return string.len(inputstring)
+end
+
+function Nova.reverse(inputstring)
+    inputstring = varstorage[inputstring] or inputstring
+    if type(inputstring) ~= "string" then
+        print("error: reverse() expects type string")
+        return
+    end
+    return string.reverse(inputstring)
+end
+
+function Nova.upper(inputstring)
+    inputstring = varstorage[inputstring] or inputstring
+    if type(inputstring) ~= "string" then
+        print("error: upper() expects type string")
+        return
+    end
+    return string.upper(inputstring)
+end
+
+function Nova.lower(inputstring)
+    inputstring = varstorage[inputstring] or inputstring
+    if type(inputstring) ~= "string" then
+        print("error: lower() expects type string")
+        return
+    end
+    return string.lower(inputstring)
 end
 
 return Nova
